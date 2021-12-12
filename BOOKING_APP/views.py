e918ebe9fd54ff45789f226ebdef21e0560a55a5
TechCentreUK <support@techcentreuk.co.uk>
2021-12-12 11:51:48 +0000

    Style my bookings page & Add sticky footer across all pages

--- BOOKING_APP/views.py
+++ BOOKING_APP/views.py
@@ -1,5 +1,5 @@
 """ Import render """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Booking
 
 # Create your views here.
@@ -12,6 +12,16 @@ def booking_app(request):
 
 def booking_view(request):
     """ Booking Page """
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        message = request.POST.get('message')
+        Booking.objects.create(name=name, email=email, phone=phone, date=date, time=time, message=message)
+
+        return redirect('my_bookings')
     return render(request, 'BOOKING_APP/booking.html')
 
 
