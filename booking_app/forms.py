c33ab8eced8a5913b1d181af40b368ae6b68ea16
TechCentreUK <support@techcentreuk.co.uk>
2022-01-03 20:58:12 +0000

    Add Restrictions to available timeslots

--- booking_app/forms.py
+++ booking_app/forms.py
@@ -4,6 +4,28 @@ from django.contrib.auth.models import User
 from .models import Booking
 
 
+DAYS_PICKER = [
+    ('Monday', 'Monday'),
+    ('Tuesday', 'Tuesday'),
+    ('Wednesday', 'Wednesday'),
+    ('Thursday', 'Thursday'),
+    ('Friday', 'Friday'),
+    ('Saturday', 'Saturday'),
+    ('Sunday', 'Sunday')
+]
+
+TIME_PICKER = [
+        ('09:00am', '09:00am'), ('09:15am', '09:15am'), ('09:30am', '09:30am'), ('09:45am', '09:45am'),
+        ('10:00am', '10:00am'), ('10:15am', '10:15am'), ('10:30am', '10:30am'), ('10:45am', '10:45am'),
+        ('11:00am', '11:00am'), ('11:15am', '11:15am'), ('11:30am', '11:30am'), ('11:45am', '11:45am'),
+        ('12:00pm', '12:00pm'), ('12:15pm', '12:15pm'), ('12:30pm', '12:30pm'), ('12:45pm', '12:45pm'),
+        ('13:00pm', '13:00pm'), ('13:15pm', '13:15pm'), ('13:30pm', '13:30pm'), ('13:45pm', '13:45pm'),
+        ('14:00pm', '14:00pm'), ('14:15pm', '14:15pm'), ('14:30pm', '14:30pm'), ('14:45pm', '14:45pm'),
+        ('15:00pm', '15:00pm'), ('15:15pm', '15:15pm'), ('15:30pm', '15:30pm'), ('15:45pm', '15:45pm'),
+        ('16:00pm', '16:00pm'), ('16:15pm', '16:15pm'), ('16:30pm', '16:30pm'), ('16:45pm', '16:45pm'),
+        ('17:00pm', '17:00pm'), ('17:15pm', '17:15pm'), ('17:30pm', '17:30pm'), ('17:45pm', '17:45pm')
+    ]
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
@@ -13,7 +35,7 @@ class BookingForm(forms.ModelForm):
     email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     date = forms.DateField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}))
-    time = forms.TimeField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'time'}))
+    time = forms.CharField(widget=forms.Select(choices=TIME_PICKER, attrs={'class': 'form-control'}))
     device = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     message = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
@@ -31,3 +53,19 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class DayPicker(forms.ModelForm):
+    
+    time = [
+        '09:00', '09:15', '09:30', '09:45',
+        '10:00', '10:15', '10:30', '10:45',
+        '11:00', '11:15', '11:30', '11:45',
+        '12:00', '12:15', '12:30', '12:45',
+        '13:00', '13:15', '13:30', '13:45',
+        '14:00', '14:15', '14:30', '14:45',
+        '15:00', '15:15', '15:30', '15:45',
+        '16:00', '16:15', '16:30', '16:45',
+        '17:00', '17:15', '17:30', '17:45',
+        '18:00'
+    ]
